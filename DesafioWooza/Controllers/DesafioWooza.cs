a1461522a0605403b693f536e30d33282e9c05ae
Rafael <rafapac@hotmail.com>
2020-03-07 12:14:05 -0300

    Commit Inicial (criação do projeto)

---
+++ DesafioWooza/Controllers/DesafioWooza.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace DesafioWooza.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DesafioWooza : ControllerBase
+    {
+
+        private readonly ILogger<DesafioWooza> _logger;
+
+        public DesafioWooza(ILogger<DesafioWooza> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>();
+        }
+    }
+}
