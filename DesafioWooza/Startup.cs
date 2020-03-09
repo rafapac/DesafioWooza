6c5e3d6a85ad4b5c6c9f7a43d4594cb9765c419e
Rafael <rafapac@hotmail.com>
2020-03-09 00:04:49 -0300

    Criação automática da base

--- DesafioWooza/Startup.cs
+++ DesafioWooza/Startup.cs
@@ -37,6 +37,8 @@ namespace DesafioWooza
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -50,6 +52,15 @@ namespace DesafioWooza
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+            context.Database.Migrate();
         }
     }
 }
